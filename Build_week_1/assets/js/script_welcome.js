2624dd08cf3d2db5e325026590b89c5b23b3c915
Giusy-Santoro <148851962+Giusy-Santoro@users.noreply.github.com>
2023-11-20 10:16:54 +0100

    Add files via upload

--- Build_week_1/assets/js/script_welcome.js
+++ Build_week_1/assets/js/script_welcome.js
@@ -1,4 +1,22 @@
-document.getElementById("button").addEventListener("click", function() {
-    // Reindirizza verso la pagina desiderata
+// Funzione che gestisce il collegamento tra checkbox e pulsante
+function checkCheckbox() {
+  let checkbox = document.getElementById("checkbox");
+  let button = document.getElementById("button");
+
+  /* Abilita o disabilita il pulsante in base allo stato della checkbox*/
+  if (checkbox.checked) {
+    button.disabled = false;
+    button.classList.add("enabled");
+  } else {
+    button.disabled = true;
+    button.classList.remove("enabled");
+  }
+}
+
+document.getElementById("button").addEventListener("click", function () {
+  let checkbox = document.getElementById("checkbox");
+
+  if (checkbox.checked) {
     window.location.href = "quiz.html";
-  });
+  }
+});
